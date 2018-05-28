b630f6cd1780bfb83d73f3998b11f6ad3a87f87e
Daniyal Maniar <mdanmaniar@gmail.com>
2018-05-27 23:08:40 -0400

    Added ability for Coinbase Connect

--- server/main.js
+++ server/main.js
@@ -4,6 +4,17 @@ import faker from 'faker';
 import { Members } from '../imports/api/members';
 
 Meteor.startup(() => {
+
+    ServiceConfiguration.configurations.upsert(
+        { service: 'coinbase' },
+        {
+            $set: {
+                loginStyle: "redirect",
+                clientId: '81111e7e9aa73ea93a8b494519c4918af0e537e4cbc337c74536e41c23d3edbe',
+                secret: '5aa3c5dd05451d3ae0d7d22dcd849b3a67e126df2885ded95603f63edb18db03',
+            },
+        },
+    );
   // code to run on server at startup
     Meteor.publish('members.allmembers',()=>{
         return Members.find();
